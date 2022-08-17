b47557d6f2a5fa80d7cf53475f37b839d54b8d43
Lis0v <kokobolo@tutanota.com>
2022-08-17 20:21:20 +0200

    balls

--- ubt.c
+++ ubt.c
@@ -1,14 +1,17 @@
 #include <stdlib.h>
+#include <stdio.h>
 
 int main(){
     
     unsigned int cur = 0x01;
+    char buf[16];
 
     for(int i=1;i<=17;i++){
         printf("\n%02x ",cur);
         cur++;
         for(int j=1;j<15;j++){
             printf("%02x ",cur);
+            printf("\u28%x",cur); //proof of concept, fix this line
             cur++;
         }
     }
